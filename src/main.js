7ab435cff0a14eb5ebccaa63bbe9837e309c3c2c
Suyeon <pisuyeun@gmail.com>
2021-07-11 01:08:56 +0900

    Create Field class

--- src/main.js
+++ src/main.js
@@ -1,13 +1,11 @@
 'use strict';
 
 import PopUp from './popup.js';
+import Field from './field.js';
 
 const gameBtn = document.querySelector('.game__button');
 const gameTimer = document.querySelector('.game__timer');
 const gameScore = document.querySelector('.game__score');
-
-const gameField = document.querySelector('.game__field');
-const fieldRect = gameField.getBoundingClientRect();
 
 const CARROT_SIZE = 80;
 const CARROT_COUNT = 10;
@@ -29,6 +27,23 @@ gameFinishBanner.setClickListener(() => {
   startGame();
 })
 
+const gameField = new Field(CARROT_COUNT, BUG_COUNT);
+gameField.setClickListener(onItemClick);
+
+function onItemClick(item) {
+  if(!started) return;
+
+  if(item === 'carrot') {
+    score++;
+    updateScoreBoard();
+    if (score === CARROT_COUNT) {
+      finishGame(true)
+    }
+  } else if (item === 'bug') {
+    playSound(bugSound)
+    finishGame(false);
+  }
+}
 gameBtn.addEventListener('click', ()=> {
   if(started){
     stopGame();
@@ -36,26 +51,6 @@ gameBtn.addEventListener('click', ()=> {
     startGame();
   }
 })
-
-gameField.addEventListener('click', onFieldClick)
-
-
-function onFieldClick(event) {
-  if(!started) return;
-  const target = event.target;
-  if(target.matches('.carrot')) {
-    target.remove();
-    score++;
-    playSound(carrotSound)
-    updateScoreBoard();
-    if (score === CARROT_COUNT) {
-      finishGame(true)
-    }
-  } else if (target.matches('.bug')) {
-    playSound(bugSound)
-    finishGame(false);
-  }
-}
 
 function startGame(){
   started = true;
@@ -140,34 +135,6 @@ function updateTimerText(time) {
 
 function initGame() {
   score = 0;
-  gameField.innerHTML = '';
   gameScore.innerText = CARROT_COUNT;
-  addItem('carrot', CARROT_COUNT, 'img/carrot.png');
-  addItem('bug', BUG_COUNT, 'img/bug.png'); 
+  gameField.init();
 }
-
-function addItem(className, count, imgPath) {
-  const x1 = 0;
-  const y1 = 0;
-  const x2 = fieldRect.width - CARROT_SIZE;
-  const y2 = fieldRect.height - CARROT_SIZE;
-  for (let i = 0; i < count; i++) {
-    const item = document.createElement('img');
-    item.setAttribute('class', className);
-    item.setAttribute('src', imgPath);
-    item.style.position = 'absolute';
-    const x = randomNumber(x1, x2);
-    const y = randomNumber(y1, y2);
-    item.style.left = `${x}px`;
-    item.style.top = `${y}px`;
-    gameField.appendChild(item);
-  }
-  
-}
-
-function randomNumber(min, max) {
-  return Math.random() * (max - min) + min;
-}
-
-
-
